e7ca23314121305389364cfc3084da6a5a9f9bbc
Vishal Singh <pintupalgot@gmail.com>
2024-09-17 00:53:10 +0530

    adding all the logic to run the game

--- index.js
+++ index.js
@@ -4,36 +4,84 @@ let score = document.querySelector("#score");
 let quads = document.querySelectorAll(".quad");
 let info = document.querySelector(".info");
 
-let level = 0;
-let winning = true;
+let scorePoints = 0;
 let gamePattern = [];
 let userPattern = [];
+let gameStarted = false;
 
+quads.forEach(quad => {
+    quad.addEventListener("click", () => {
+        // Ignore clicks if the game hasn't started
+        if (!gameStarted) return;
 
-play.addEventListener("click",() => {
+        let index;
+        if (quad.classList.contains("quad1")) index = 0;
+        else if (quad.classList.contains("quad2")) index = 1;
+        else if (quad.classList.contains("quad3")) index = 2;
+        else if (quad.classList.contains("quad4")) index = 3;
+
+        if (index !== undefined) {
+            userPattern.push(index);
+            quad.classList.add("pressed");
+            setTimeout(() => quad.classList.remove("pressed"), 500);
+
+            checkPattern();
+        }
+    });
+});
+
+play.addEventListener("click", () => {
     gameStarted = true;
     info.classList.add("hide");
     play.classList.add("hide");
     reset.classList.remove("hide");
+    gameRun();
+});
 
-    function gameRun(winning) {
-        level++;
-        gamePattern.push(Math.floor(Math.random() * 4));
-        showPattern();
-
-    }
-
-    quads.forEach(quad => {
-        quad.addEventListener("click",() => {
-            
-        })
-    })
-
-})
-
+function gameRun() {
+    scorePoints++;
+    score.innerHTML = "Score : " + scorePoints;
+    gamePattern.push(Math.floor(Math.random() * 4));
+    userPattern = [];
+    showPattern();
+}
 
 function showPattern() {
-    gamePattern.forEach(pattern => {
-        quads[pattern].classList.add("pressed");
-    })
+    gameStarted = false;  // Disable clicks during pattern display
+    gamePattern.forEach((pattern, index) => {
+        setTimeout(() => {
+            quads[pattern].classList.add("pressed");
+            setTimeout(() => quads[pattern].classList.remove("pressed"), 300);
+
+            if (index === gamePattern.length - 1) {
+                setTimeout(() => {
+                    gameStarted = true; 
+                }, 300); 
+            }
+        }, index * 600);
+    });
+}
+
+function checkPattern() {
+    for (let i = 0; i < userPattern.length; i++) {
+        if (gamePattern[i] !== userPattern[i]) {
+            resetGame();
+            return;
+        }
+    }
+    
+    if (userPattern.length === gamePattern.length) {
+        setTimeout(gameRun, 1000);
+    }
+}
+
+function resetGame() {
+    gameStarted = false;
+    info.classList.remove("hide");
+    play.classList.remove("hide");
+    reset.classList.add("hide");
+    scorePoints = 0;
+    score.innerHTML = "Score : 0";
+    gamePattern = [];
+    userPattern = [];
 }
